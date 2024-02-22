05facbd64908ae22be51611934254a080db2ecb7
Alex <alexvdvelden@hotmail.com>
2024-02-22 16:50:15 +0100

    added a CollectionView, defined data template and added an observableCollection

--- MauiToDo/MainPage.xaml.cs
+++ MauiToDo/MainPage.xaml.cs
@@ -1,25 +1,51 @@
-﻿namespace MauiToDo
+﻿using MauiTodo.Data;
+using MauiTodo.Models;
+using System.Collections.ObjectModel;
+
+namespace MauiTodo;
+
+public partial class MainPage : ContentPage
 {
-    public partial class MainPage : ContentPage
+    public ObservableCollection<TodoItem> Todos { get; set; } = new();
+
+    readonly Database _database;
+
+    public MainPage()
     {
-        int count = 0;
+        InitializeComponent();
+        _database = new Database();
 
-        public MainPage()
+        _ = Initialize();
+
+        TodosCollection.ItemsSource = Todos;
+    }
+
+    private async Task Initialize()
+    {
+        var todos = await _database.GetTodos();
+
+        foreach (var todo in todos)
         {
-            InitializeComponent();
-        }
-
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            Todos.Add(todo);
         }
     }
 
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+        var todo = new TodoItem
+        {
+            Due = DueDatepicker.Date,
+            Title = TodoTitleEntry.Text
+        };
+
+        var inserted = await _database.AddTodo(todo);
+
+        if (inserted != 0)
+        {
+            Todos.Add(todo);
+
+            TodoTitleEntry.Text = String.Empty;
+            DueDatepicker.Date = DateTime.Now;
+        }
+    }
 }
---
+++ MauiTodo/Models/ToDoItem.cs
@@ -0,0 +1,16 @@
+﻿using SQLite;
+
+namespace MauiTodo.Models
+{
+    public class TodoItem
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public DateTime Due { get; set; }
+
+        public bool Done { get; set; } = false;
+    }
+}
