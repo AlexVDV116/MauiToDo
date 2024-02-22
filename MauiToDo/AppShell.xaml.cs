4d7bbdff4565becc230901a6f481c27b66a7fc53
Alex <alexvdvelden@hotmail.com>
2024-02-22 14:58:53 +0100

    Add project files.

---
+++ MauiToDo/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiToDo
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MauiToDo/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiToDo
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
